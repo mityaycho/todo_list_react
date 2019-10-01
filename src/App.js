a5f55e2ecdff19e6486f5e232c6b47429f1ce2ea
mityaycho <mityaycho@gmail.com>
2019-10-01 14:15:58 +0300

    firct commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+    render = () => {
+        return (
+            <div className="App">
+                <div className="todoList">
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
