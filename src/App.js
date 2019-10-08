b718b9d58ecf4431ab15d006d3ff16aa12e4afca
mityaycho <mityaycho@gmail.com>
2019-10-08 21:08:33 +0300

    add state in class component

--- src/App.js
+++ src/App.js
@@ -5,26 +5,53 @@ import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
-    tasks = [
-        {title: "CSS", priority: "high", isDone: true},
-        {title: "JS", priority: "low", isDone: false},
-        {title: "ReactJS", priority: "low", isDone: false},
-        {title: "Patterns", priority: "high", isDone: true}
-    ];
+  constructor(props) {
+    super(props);
+    this.newTaskTitleRef = React.createRef();
+  };
 
-    filterValue = 'All';
+  state = {
+    tasks: [
+      {title: "CSS", priority: "high", isDone: true},
+      {title: "JS", priority: "low", isDone: false},
+      {title: "ReactJS", priority: "low", isDone: false},
+      {title: "Patterns", priority: "high", isDone: true}
+    ],
+    filterValue: "Active"
+  };
 
-    render = () => {
-        return (
-            <div className="App">
-                <div className="todoList">
-                    <TodoListHeader />
-                    <TodoListTasks tasks={this.tasks} />
-                    <TodoListFooter filterValue={this.filterValue}/>
-                </div>
+  onAddTaskClick = () => {
+    let newTask = {
+        title: this.newTaskTitleRef.current.value,
+        priority: "high",
+        isDone: false
+    };
+    this.newTaskTitleRef.current.value = '';
+    let newTasks = [...this.state.tasks, newTask];
+    this.setState({
+      tasks: newTasks
+    })
+  };
+
+  render = () => {
+    return (
+      <div className="App">
+        <div className="todoList">
+          <div className="todoList-header">
+            <h3 className="todoList-header__title">What to Learn</h3>
+            <div className="todoList-newTaskForm">
+              <input ref={this.newTaskTitleRef} type="text" placeholder="New task name"/>
+              <button onClick={this.onAddTaskClick}>Add
+              </button>
             </div>
-        );
-    }
+          </div>
+          {/*<TodoListHeader />*/}
+          <TodoListTasks tasks={this.state.tasks}/>
+          <TodoListFooter filterValue={this.state.filterValue}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
