81382deecf17e0d93c10e9e644870982574ca95b
mityaycho <mityaycho@gmail.com>
2019-11-21 23:26:53 +0300

    add some changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -10,7 +10,7 @@ class TodoList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.newTasksTitileRef = React.createRef();
+        // this.newTasksTitileRef = React.createRef();
 
     }
 
