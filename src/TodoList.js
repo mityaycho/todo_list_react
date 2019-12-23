3223620c3dea7ca92e4558a4baf83a3bda98b928
mityaycho <mityaycho@gmail.com>
2019-12-23 22:42:09 +0300

    add some changes

--- src/TodoList.js
+++ src/TodoList.js
@@ -85,22 +85,24 @@ class TodoList extends React.Component {
           <TodoListTitle title={this.props.title} onDelete={this.deleteTodolist} updateTitle={this.updateTitle}/>
           <AddNewItemForm addItem={this.addTask} placeholder="New task" />
         </main>
-        <TodoListFilter changeFilter={this.changeFilter} filterValue={this.state.filterValue}/>
-        <TodoListTasks changeStatus={this.changeStatus}
-                       changeTitle={this.changeTitle}
-                       deleteTask={this.deleteTask}
-          /*tasks={this.props.tasks.filter(t => {*/
-                       tasks={tasks.filter(t => {
-                         if (this.state.filterValue === "All") {
-                           return true;
-                         }
-                         if (this.state.filterValue === "Active") {
-                           return t.status === 0;
-                         }
-                         if (this.state.filterValue === "Completed") {
-                           return t.status === 2;
-                         }
-                       })}/>
+        <section className="section-tasks">
+          <TodoListFilter changeFilter={this.changeFilter} filterValue={this.state.filterValue}/>
+          <TodoListTasks changeStatus={this.changeStatus}
+                         changeTitle={this.changeTitle}
+                         deleteTask={this.deleteTask}
+            /*tasks={this.props.tasks.filter(t => {*/
+                         tasks={tasks.filter(t => {
+                           if (this.state.filterValue === "All") {
+                             return true;
+                           }
+                           if (this.state.filterValue === "Active") {
+                             return t.status === 0;
+                           }
+                           if (this.state.filterValue === "Completed") {
+                             return t.status === 2;
+                           }
+                         })}/>
+        </section>
       </div>
     );
   }
