e21fb6f3a94edb06b1b424064bcd3765af281bb8
mityaycho <mityaycho@gmail.com>
2020-06-24 23:01:29 +0300

    Updates

---
+++ precache-manifest.9efd77cb22364836decbfa7e860f0ba6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8d0c72c0c28d6e5d108c26b5f528f6",
+    "url": "/todo_list_react/index.html"
+  },
+  {
+    "revision": "b639e32f611fd80ebb4a",
+    "url": "/todo_list_react/static/css/main.6fc078b5.chunk.css"
+  },
+  {
+    "revision": "c5c53477275e0fcff044",
+    "url": "/todo_list_react/static/js/2.86353052.chunk.js"
+  },
+  {
+    "revision": "9baae3bc79debe09706936a65b6142d0",
+    "url": "/todo_list_react/static/js/2.86353052.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b639e32f611fd80ebb4a",
+    "url": "/todo_list_react/static/js/main.6c1b2f52.chunk.js"
+  },
+  {
+    "revision": "bb44da4e9d3eed557b1c",
+    "url": "/todo_list_react/static/js/runtime-main.8c9f9a68.js"
+  }
+]);
